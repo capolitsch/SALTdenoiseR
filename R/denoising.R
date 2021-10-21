7c9ad194aca3509b913e61074002f5cfbce243ec
Collin A. Politsch <capolitsch@cmu.edu>
2021-10-21 00:43:23 -0400

    Update documentation

--- R/denoising.R
+++ R/denoising.R
@@ -4,60 +4,24 @@
 #' @param stokes Spectropolarimetric measurements in a Stokes parametrization,
 #' passed as a 3-column matrix, with the columns corresponding to the I, Q, U
 #' Stokes parameters, respectively.
-#' @param var Measurement variances, in a matrix with dimensions matching those
-#' of `stokes`.
-#' @param pixel_masks Pixel masks, in a matrix with dimensions matching those
-#' of `stokes`.
-#' @param spectrum_break Parameter that controls the segmentation of a spectrum
+#' @param variances Measurement variances, in a matrix with dimensions matching
+#' those of `stokes`.
+#' @param masks Pixel masks, in a matrix with dimensions matching those
+#' of `stokes`. Nonzero elements flag bad pixels.
+#' @param spectrum_break Parameter controlling the segmentation of a spectrum
 #' into ``sub-spectra'', which are then denoised independently. More precisely,
 #' `spectrum_break` is the minimum number of consecutively-masked spectral
-#' pixels in order to trigger a break in the spectrum. Defaults to
-#' `spectrum_break = 20`.
-#' @param nx_eval Integer. If nothing is passed to `x_eval`, then it is defined
-#' as `x_eval = seq(min(x), max(x), length = nx_eval)`.
-#' @param x_eval (Optional) A grid of inputs to evaluate the optimized trend
-#' filtering estimate on. May be ignored, in which case the grid is determined
-#' by `nx_eval`.
+#' pixels required to trigger a break in the spectrum. Defaults to
+#' `spectrum_break = 10`.
 #' @param compute_uncertainties (Boolean) If `TRUE` then variability bands are
-#' computed via a parametric bootstrap algorithm based on the uncertainties
-#' in the spectral pixel measurements.
-#' @param B (Integer) The number of bootstrap samples that are drawn in order to
-#' estimate the trend filtering variability bands. Larger is better, but is more
-#' computationally expensive. My trend filtering bootstrap implementation is set
-#' up for parallel computing for speedups when multiple cores are available.
-#' `B = 100` typically suffices for a most scenarios, but it's good to crank it
-#' up a bit for a plot that's going to be published, so you get nice smooth
-#' bands.
-#' @return An object of class [`'SALT_spec'`][denoise_spectrum()]. This is a
-#' list with the following elements:
-#' \item{x_eval}{Input grid used to evaluate the optimized trend filtering
-#' estimate on.}
-#' \item{tf_estimate}{Optimized trend filtering estimate, evaluated at
-#' `x_eval`.}
-#' \item{validation_method}{"SURE"}
-#' \item{lambdas}{Vector of hyperparameter values evaluated in the grid search
-#' (always returned in descending order).}
-#' \item{edfs}{Vector of effective degrees of freedom for all trend filtering
-#' estimators fit during validation.}
-#' \item{generalization_errors}{Vector of SURE generalization error estimates,
-#' corresponding to the descending-ordered `lambdas` vector.}
-#' \item{lambda_min}{Hyperparameter value that minimizes the SURE generalization
-#' error curve.}
-#' \item{i_min}{Index of `lambdas` that minimizes the SURE error curve.}
-#' \item{edf_min}{Effective degrees of freedom of the optimized trend
-#' filtering estimator.}
-#' \item{n_iter}{The number of iterations needed for the ADMM algorithm to
-#' converge within the given tolerance, for each hyperparameter value. If many
-#' of these are exactly equal to `max_iter`, then their solutions have not
-#' converged with the tolerance specified by `obj_tol`. In which case, it is
-#' often prudent to increase `max_iter`.}
-#' \item{x}{Vector of observed inputs.}
-#' \item{y}{Vector of observed outputs.}
-#' \item{weights}{Weights for the observed outputs, defined as the reciprocal
-#' variance of the additive noise that contaminates the signal.}
-#' \item{fitted_values}{Optimized trend filtering estimate, evaluated at the
-#' observed inputs `x`.}
-#' \item{residuals}{`residuals = y - fitted_values`}
+#' computed for each of the denoised normalized Stokes spectra, via a parametric
+#' bootstrap algorithm.
+#' @param ... Additional named arguments to be passed to
+#' [trendfiltering::sure_trendfilter()] or
+#' [trendfiltering::bootstrap_trendfilter()].
+#' @return An object of class
+#' [`'polarized_spectrum_denoised'`][denoise_polarized_spectrum()].
+#' This is a list with the following elements:
 #'
 #' @export denoise_polarized_spectrum
 #'
@@ -92,8 +56,8 @@
 #' @importFrom tidyr drop_na tibble as_tibble
 #' @importFrom dplyr %>% arrange filter select n_distinct bind_cols
 #' @importFrom magrittr %$%
-denoise_polarized_spectrum <- function(wavelength, stokes, weights, masks,
-                                       spectrum_break = 20,
+denoise_polarized_spectrum <- function(wavelength, stokes, variances, masks,
+                                       spectrum_break = 10,
                                        compute_uncertainties = FALSE,
                                        ...) {
   wavelength <- as_tibble_col(wavelength, column_name = "wavelength")
